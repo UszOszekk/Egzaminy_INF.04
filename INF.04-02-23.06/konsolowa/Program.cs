1f4326e3e604797c13502e5605335a11a83f0d5b
Mateusz <92518862+UszOszekk@users.noreply.github.com>
2026-01-20 17:45:18 +0100

    mobilna 2023

---
+++ INF.04-02-23.06/konsolowa/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace INF._04_02_23._06_SG_kjonsola_
+{
+    internal class Program
+    {
+
+        public static void sortuj(int[] tablica)
+        {
+            for(int i = 0; i < tablica.Length; i++)
+            {
+                for(int j = i; j < tablica.Length; j++)
+                {
+                    if (tablica[i] > tablica[j])
+                    {
+                        int temp = tablica[i];
+                        tablica[i] = tablica[j];
+                        tablica[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random losowanie = new Random();
+
+            
+
+            int[] tablicaLiczb = new int[100];
+
+            for(int i = 0; i < tablicaLiczb.Length; i++)
+            {
+                int losowaLiczba = losowanie.Next(0, 1000);
+                tablicaLiczb[i] = losowaLiczba;
+            }
+            Console.WriteLine("Tablica przed posortowaniem:");
+            for (int i = 0; i < tablicaLiczb.Length; i++)
+            {
+                Console.Write(tablicaLiczb[i] + ", ");
+            }
+            sortuj(tablicaLiczb);
+            Console.WriteLine("\nTablica PO posortowaniem:");
+            for (int i = 0; i < tablicaLiczb.Length; i++)
+            {
+                Console.Write(tablicaLiczb[i] + ", ");
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ INF.04-02-23.06/mobilna/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace INF._04_02_23._06_SG_mobile_
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+           double wartoscSLidera = Math.Round(slider2.Value);
+            rozmiarText.Text = "Rozmiar: "+  wartoscSLidera;
+            Cytat.FontSize = wartoscSLidera;
+        }
+
+        string[] przywitania = { "Dzień dobry", "Good morning","Buenos dias"};
+        int nrPrzywitania = 0;
+
+        private void przycisk_Clicked(object sender, EventArgs e)
+        {
+
+            nrPrzywitania++;
+            if(nrPrzywitania == 3)
+            {
+                nrPrzywitania = 0;
+            }
+            Cytat.Text = przywitania[nrPrzywitania].ToString();
+
+        }
+    }
+}
